8a170c1b1eb3d9812c8832a1b1a56c10e541c074
Gurminder Singh <gurmindersingh@Gurminders-MacBook-Pro.local>
2023-02-03 12:00:51 -0500

    Added Infrastructure project

---
+++ src/Authentication.Infrastructure/AuthenticationDIRegistration.cs
@@ -0,0 +1,16 @@
+﻿namespace Authentication.Infrastructure
+{
+    using Authentication.Services;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class AuthenticationDIRegistration
+    {
+        public static IServiceCollection AddAuthenticationServices(this IServiceCollection services)
+        {
+            services.AddServices();
+            return services;
+        }
+    }
+}
+
